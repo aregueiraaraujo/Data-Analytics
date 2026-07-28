50243c1cf18a3d005e001b6978f197e21b73b596
aregueiraaraujo <a.regueiraaraujo@gmail.com>
2026-07-28 10:22:25 +0200

    Update Nivel 1 - 6.sql

--- Sprint2/Nivel 1 - 6.sql
+++ Sprint2/Nivel 1 - 6.sql
@@ -1,11 +1,12 @@
-# INSERT INTO company (Id) 
-# VALUES ('b-9999');
+INSERT IGNORE INTO company (id) 
+VALUES ('b-9999');
 
-# INSERT INTO transaction (Id, credit_card_id, company_id, user_id, Lat, Longitude, Amount, Declined) 
-# VALUES ('108B1D1D-5B23-A76C-55EF-C568E49A99DD', 'CcU-9999', 'b-9999', '9999', '829.999', '-117.999', '111.11', '0');
+INSERT INTO credit_card (id, iban, pan, pin, cvv, expiring_date) 
+VALUES ('CcU-9999', 'ES1234567890123456789012', '1234567812345678', '1234', '123', '12/2028');
+
+INSERT INTO transaction (id, credit_card_id, company_id, user_id, lat, longitude, amount, declined) 
+VALUES ('108B1D1D-5B23-A76C-55EF-C568E49A99DD', 'CcU-9999', 'b-9999', '9999', '829.999', '-117.999', '111.11', '0');
 
 SELECT * 
 FROM transaction
 WHERE id = '108B1D1D-5B23-A76C-55EF-C568E49A99DD';
-
-
