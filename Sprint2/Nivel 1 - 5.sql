e8422543cb6a4b469955f76d15227f1a9a13ac8f
aregueiraaraujo <a.regueiraaraujo@gmail.com>
2026-07-28 10:20:39 +0200

    Update Nivel 1 - 5.sql

--- Sprint2/Nivel 1 - 5.sql
+++ Sprint2/Nivel 1 - 5.sql
@@ -1,8 +1,7 @@
-# UPDATE credit_card
-# SET iban = 'TR323456312213576817699999' 
-# WHERE id = 'CcU-2938';
+UPDATE credit_card
+SET iban = 'TR323456312213576817699999' 
+WHERE id = 'CcU-2938';
 
 SELECT iban 
 FROM credit_card
 WHERE id = 'CcU-2938';
-
