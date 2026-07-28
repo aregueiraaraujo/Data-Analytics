d722a82ddb4af88f5053da53083f1becfece01d6
aregueiraaraujo <a.regueiraaraujo@gmail.com>
2026-07-28 10:17:25 +0200

    Update Nivel 1 - 3.2.sql

--- Sprint2/Nivel 1 - 3.2.sql
+++ Sprint2/Nivel 1 - 3.2.sql
@@ -1,7 +1,7 @@
 SELECT amount, (
 	SELECT company_name 
-	FROM company
-    WHERE company.id = transaction.company_id
+	FROM company co
+    WHERE co.id = t.company_id AND t.declined = 0 
 	) AS Company_name
-FROM transaction
+FROM transaction t
 WHERE amount > (SELECT AVG(amount) FROM transaction);
