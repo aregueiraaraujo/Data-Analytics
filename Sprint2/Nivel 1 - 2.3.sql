809080e81ff487ee754f84090391df05b5158214
aregueiraaraujo <a.regueiraaraujo@gmail.com>
2026-07-28 10:16:38 +0200

    Update Nivel 1 - 2.3.sql

--- Sprint2/Nivel 1 - 2.3.sql
+++ Sprint2/Nivel 1 - 2.3.sql
@@ -2,7 +2,8 @@ SELECT company.company_name, ROUND(AVG(amount),2) AS media_de_ventas FROM compan
 JOIN transaction
 ON company.id = transaction.company_id
 WHERE transaction.declined = 0
-GROUP BY company.company_name
+GROUP BY company.id
 ORDER BY media_de_ventas DESC
 LIMIT 1;
+
 
