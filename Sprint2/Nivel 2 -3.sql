a81dfadef0db249a63faf6645a06c16c87dc775a
aregueiraaraujo <a.regueiraaraujo@gmail.com>
2026-07-28 10:28:38 +0200

    Update Nivel 2 -3.sql

--- Sprint2/Nivel 2 -3.sql
+++ Sprint2/Nivel 2 -3.sql
@@ -1,11 +1,10 @@
-SELECT 
-    co.company_name, 
-    COUNT(t.id) AS transaciones,
-    CASE 
-        WHEN COUNT(t.id) >= 400 THEN 'Igual o más de 400 transaciones'
-        ELSE 'Menos de 400 transaciones'
-    END AS clasificacion_ventas
+SELECT co.company_name, COUNT(t.id) AS transacciones,
+CASE 
+	WHEN COUNT(t.id) >= 400 THEN 'Igual o más de 400 transaciones'
+	ELSE 'Menos de 400 transaciones'
+END AS clasificacion_ventas
 FROM companies AS co
 JOIN transactions AS t ON co.company_id = t.business_id
+WHERE t.declined = 0
 GROUP BY co.company_id, co.company_name
-ORDER BY transaciones DESC;
+ORDER BY transacciones;
