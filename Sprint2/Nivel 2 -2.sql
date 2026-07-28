d6935408a757dd1bde91d511b8a156d2fca94c23
aregueiraaraujo <a.regueiraaraujo@gmail.com>
2026-07-28 10:27:40 +0200

    Update Nivel 2 -2.sql

--- Sprint2/Nivel 2 -2.sql
+++ Sprint2/Nivel 2 -2.sql
@@ -1,9 +1,7 @@
-SELECT co.company_name, co.phone, co.country, t.time_stamp, t.amount
+SELECT co.company_name, co.phone, co.country, DATE(t.time_stamp), t.amount
 FROM companies AS co
 JOIN transactions AS t
 ON t.business_id = co.company_id
-WHERE t.amount >350 AND t.amount <400 AND (
-CAST(time_stamp AS DATE) = '2015-04-29' 
-OR CAST(time_stamp AS DATE) = '2018-07-20' 
-OR CAST(time_stamp AS DATE) = '2024-03-13')
+WHERE t.amount >350 AND t.amount <400 AND
+DATE(time_stamp) IN ('2015-04-29', '2018-07-20', '2024-03-13')
 ORDER BY amount ASC;
